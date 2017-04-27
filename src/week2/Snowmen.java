78dfc9d803c5183884069703d5276367fae586cd
sherxon <sheraliobidov1199@gmail.com>
2017-04-26 23:22:56 -0500

    temp files

--- src/week2/Snowmen.java
+++ src/week2/Snowmen.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
  * Created by sherxon on 3/16/17.
  */
 public class Snowmen {
+
     public static void main(String[] args) throws IOException {
         try (PrintWriter out = newOutput()) {
             FastScanner in = newInput();
